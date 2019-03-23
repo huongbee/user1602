ae8e78df032b46225a13adcb19c448e74075b35a
Huong <huongnguyenak96@gmail.com>
2019-03-23 16:26:26 +0700

    jwt

--- controller/user.route.js
+++ controller/user.route.js
@@ -36,5 +36,37 @@ route.post('/register',(req,res)=>{
 route.get('/login',(req,res)=>{
     res.render('login');
 })
-
+route.post('/login',(req,res)=>{
+    const { email , password } = req.body
+    //find user by email
+    UserModel.findOne({email})
+    .then(user=>{
+        if(!user){
+            req.flash('error_message','Cannot find user 1')
+            return res.redirect('/user/login')
+        }
+        else{
+            // compare password
+            compare(password,user.password)
+            .then(result=>{
+                if(result){
+                    // login in success
+                    // jwt 
+                       
+                    // save cookie
+                    res.send(user)
+                }
+                else{
+                    req.flash('error_message','Password invalid!')
+                    return res.redirect('/user/login')
+                }
+            })            
+        }
+    })
+    .catch((err)=>{
+        console.log(err)
+        req.flash('error_message','Cannot find user 2')
+        return res.redirect('/user/login')
+    })
+})
 module.exports = route
